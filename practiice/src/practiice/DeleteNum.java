f974b19d48e5b65f3b7c983eab2b2588296fcc4f
tkysn1028 <tkysn1028@gmail.com>
2021-08-30 07:12:34 +0900

    簡単な電卓

---
+++ practiice/src/practiice/DeleteNum.java
@@ -0,0 +1,38 @@
+package practiice;
+
+public class DeleteNum {
+
+	
+	
+	
+	
+	
+	
+	
+	public StringBuilder deletenum(StringBuilder sb) {	
+		
+		
+		if(sb.toString().length() == 1) {
+			sb.delete(0, 1);
+			sb.append("0");
+			
+		}else {
+		
+		sb.delete(sb.toString().length() - 1, sb.toString().length());
+		}
+		return sb;
+	
+	}
+	
+	public static void main(String[] args) {
+		
+		DeleteNum num = new DeleteNum();
+		StringBuilder sb = new StringBuilder("4");
+		num.deletenum(sb);
+		
+		System.out.println(sb.toString());
+	}
+	
+	
+	
+}
